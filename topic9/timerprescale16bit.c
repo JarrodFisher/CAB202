b7f50df634c6ba8e78eb1e4b5ff3268f760cbacb
Jarrod Fisher <n10462091@qut.edu.au>
2021-05-10 16:02:39 +1000

    Created files with test drivers for all tasks

---
+++ topic9/timerprescale16bit.c
@@ -0,0 +1,31 @@
+// Replace 0 with the correct value for each unknown cell.
+double prescale_001 = 0;
+double prescale_010 = 0;
+double prescale_011 = 0;
+double prescale_100 = 0;
+double prescale_101 = 0;
+double count_frequency_001 = 0;
+double count_frequency_010 = 0;
+double count_frequency_011 = 0;
+double count_frequency_100 = 0;
+double count_frequency_101 = 0;
+double overflow_period_001 = 0;
+double overflow_period_010 = 0;
+double overflow_period_011 = 0;
+double overflow_period_100 = 0;
+double overflow_period_101 = 0;
+double overflow_freq_001 = 0;
+double overflow_freq_010 = 0;
+double overflow_freq_011 = 0;
+double overflow_freq_100 = 0;
+double overflow_freq_101 = 0;
+
+
+// There is no test driver for this program.
+
+int main() {
+	for ( ;; ) {
+	}
+
+	return 0;
+}
